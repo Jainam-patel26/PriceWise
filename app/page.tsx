647f99261c06d6abb137170bb6257899397a2edd
justcodingxdd <pateljainam25@gmail.com>
2024-06-04 20:29:04 +0530

    ProductCard

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import Image from 'next/image'
 import Searchbar from '@/components/Searchbar'
 import HeroCarousel from '@/components/HeroCarousel'
 import { getAllProducts } from '../lib/actions'
+import ProductCard from '@/components/ProductCard'
 
-const page = () => {
+const page = async () => {
+
+  const allProducts  = await getAllProducts();
+
   return (
     <>
       <section className='px-6  md:px-20 pt-2 pb-24 mx-auto'>
@@ -43,9 +47,8 @@ const page = () => {
         <h2 className='text-[32px] font-semibold'>Trending</h2>
 
         <div className='flex flex-wrap gap-x-8 gap-y-16'>
-          {['Apple Iphone 15', 'Book', 'Sneakers'].map 
-          ((product) => (
-            <div>{product}</div>
+          {allProducts?.map ((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
